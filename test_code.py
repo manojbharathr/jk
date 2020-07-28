6032ea2c3cf9f91478ee9b6a3b80c99509769a28
mravuri <manojbharath.ravuri@valpo.edu>
2020-07-27 20:41:22 -0500

    initial commit

---
+++ test_code.py
@@ -0,0 +1,11 @@
+import module 
+
+def test_answer():
+    assert module.inc(3) == 4
+
+def test_answer2():
+    assert module.inc(90) == 91
+
+
+def test_answer3():
+    assert module.inc(30) == 22
